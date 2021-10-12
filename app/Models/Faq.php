37a49ab5607abbddc6417ee224daa47113ab3582
KOMLANI Tibang Tibang\'be <tibang.kolani@gmail.com>
2021-10-12 22:39:50 +0000

    Update

---
+++ app/Models/Faq.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Faq
+ * 
+ * @property int $id
+ * @property int $item_id
+ * @property string $slugable
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ * @property Item $item
+ *
+ * @package App\Models
+ */
+class Faq extends Model
+{
+	protected $table = 'faqs';
+
+	protected $casts = [
+		'item_id' => 'int'
+	];
+
+	protected $fillable = [
+		'item_id',
+		'slugable'
+	];
+
+	public function item()
+	{
+		return $this->belongsTo(Item::class);
+	}
+}
